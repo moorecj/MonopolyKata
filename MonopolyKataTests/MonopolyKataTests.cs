ca9d2351edd22f75ad9d4e8e5e92ed0f4b7bb9ed
moorecj <moore.cyril@gmail.com>
2014-06-23 11:10:04 -0400

    Too many players test fails.

--- MonopolyKataTests/MonopolyKataTests.cs
+++ MonopolyKataTests/MonopolyKataTests.cs
@@ -16,11 +16,39 @@ namespace MonopolyKataTests
         public void CanMakeNewMonopolyGameWith2Players()
         {
             Player player1 = new Player();
-            Player Player2 = new Player();
+            Player player2 = new Player();
 
             List<Player> players = new List<Player>();
 
+            players.Add(player1);
+            players.Add(player2);
+
             Monopoly game = new Monopoly(players);
+
+            Assert.That(game, Is.Not.Null);
+        }
+
+        [Test]
+        public void AGameWithMoreThen8PlayersShouldFail()
+        {
+            
+
+            List<Player> players = new List<Player>();
+
+            for (int i = 0; i <= 9; ++i )
+            {
+                players.Add(new Player());
+            }
+
+
+            Monopoly game = new Monopoly(players);
+
+            var exception = Assert.Throws<TooManyPlayersException>(() => new Monopoly(players));
+
+            Assert.That(exception.Message, Is.EqualTo("Too many players: 9"));
+
         }
     }
+
+
 }
