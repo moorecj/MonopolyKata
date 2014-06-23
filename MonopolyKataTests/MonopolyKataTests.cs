88057162b43e020157d504c72e18a8de54ab48dd
moorecj <moore.cyril@gmail.com>
2014-06-23 10:38:17 -0400

    Initial commit to the Monopoly Kata

---
+++ MonopolyKataTests/MonopolyKataTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonopolyKata;
+
+using NUnit.Framework;
+
+namespace MonopolyKataTests
+{
+    [TestFixture]
+    public class MonopolyKataTests
+    {
+        [Test]
+        public void CanMakeNewMonopolyGameWith2Players()
+        {
+            Player player1 = new Player();
+            Player Player2 = new Player();
+
+            List<Player> players = new List<Player>();
+
+            Monopoly game = new Monopoly(players);
+        }
+    }
+}
