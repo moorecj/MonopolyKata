af704475b26efc06ed57ff7a25e47b9d7e8012b0
Cyril Moore <cmoore@foxguardsolutions.com>
2014-08-04 10:56:46 -0400

    Subtract funds "when drawn" strategy, tests pass.

--- MonopolyKataTests/CardTests/WhenDrawnTests/SubtractFundsTests.cs
+++ MonopolyKataTests/CardTests/WhenDrawnTests/SubtractFundsTests.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using MonopolyKata;
+using MonopolyKata.Cards.WhenDrawnStrategies;
 
 namespace MonopolyKataTests.CardTests.WhenDrawnTests
 {
-    class SubtractFundsTests
+    [TestFixture]
+    public class SubtractFundsTests
     {
+        [Test]
+        public void TheSubtractFundsStrategyWillTakeAnAmmount()
+        {
+            int amountToSubtract = 10;
+            SubtractFundsStrategy subtractStrategy = new SubtractFundsStrategy(amountToSubtract);
+
+            Assert.That(subtractStrategy, Is.Not.Null);
+        }
+
+        [Test]
+        public void TheSubtractFundsStrategyWillSubtractThePastAmountFromTheBalenceWhenApplied()
+        {
+            int amountToSubtract = 10;
+            SubtractFundsStrategy subtractStrategy = new SubtractFundsStrategy(amountToSubtract);
+            
+            MonopolyPlayer player = new MonopolyPlayer("Player");
+
+            player.Balence = amountToSubtract;
+
+            subtractStrategy.Apply(player);
+
+            Assert.That(player.Balence, Is.EqualTo(0));
+        }
+
+
     }
 }
