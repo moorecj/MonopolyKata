1bdd693562b43d7f45dde328e9c4009112e6f07e
Cyril Moore <cmoore@foxguardsolutions.com>
2014-08-04 10:40:02 -0400

    Add funds strategy takes an amount, test passes.

---
+++ MonopolyKata/Cards/WhenDrawnStrategies/AddFundsStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyKata.Cards.WhenDrawnStrategies
+{
+    public class AddFundsStrategy : IWhenDrawnStrategy
+    {
+        private int amount;
+
+        public AddFundsStrategy(int amount )
+        {
+            this.amount = amount;
+        }
+
+        public void Apply(MonopolyPlayer player)
+        {
+            player.Balence += amount;
+        }
+    }
+}
--- MonopolyKataTests/CardTests/WhenDrawnTests/AddFundsTests.cs
+++ MonopolyKataTests/CardTests/WhenDrawnTests/AddFundsTests.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using MonopolyKata.Cards.WhenDrawnStrategies;
 
 namespace MonopolyKataTests.CardTests.WhenDrawnTests
 {
-    class AddFundsTests
+    [TestFixture]
+    public class AddFundsTests
     {
+        [Test]
+        public void TheAddFundsStrategyWillTakeAnAmmount()
+        {
+            int amountToAdd = 10;
+            AddFundsStrategy addStrategy = new AddFundsStrategy(amountToAdd);
+
+            Assert.That(addStrategy, Is.Not.Null);
+        }
     }
 }
