2e440892a24aa00bc12278d85630e66ea12d3a83
Cyril Moore <cmoore@foxguardsolutions.com>
2014-08-04 10:48:20 -0400

    Apply add amount strategy, test passes;

--- MonopolyKataTests/CardTests/WhenDrawnTests/AddFundsTests.cs
+++ MonopolyKataTests/CardTests/WhenDrawnTests/AddFundsTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using MonopolyKata;
 using MonopolyKata.Cards.WhenDrawnStrategies;
 
 namespace MonopolyKataTests.CardTests.WhenDrawnTests
@@ -19,5 +20,22 @@ namespace MonopolyKataTests.CardTests.WhenDrawnTests
 
             Assert.That(addStrategy, Is.Not.Null);
         }
+
+        [Test]
+        public void TheAddFundsStrategyWillAddThePastAmountToThePlayerWhenApplied()
+        {
+            int amountToAdd = 10;
+            AddFundsStrategy addStrategy = new AddFundsStrategy(amountToAdd);
+            
+            MonopolyPlayer player = new MonopolyPlayer("Player");
+
+            Assert.That(player.Balence, Is.EqualTo(0));
+
+            addStrategy.Apply(player);
+
+            Assert.That(player.Balence, Is.EqualTo(amountToAdd));
+        }
+
+
     }
 }
