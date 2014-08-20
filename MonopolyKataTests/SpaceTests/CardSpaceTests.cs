325db34e307378ce251b07bf7fb917d044ed3036
Cyril Moore <moore.cyril@gmail.com>
2014-08-19 23:16:03 -0400

    Progress on DeckSpaceTests

--- MonopolyKataTests/SpaceTests/CardSpaceTests.cs
+++ MonopolyKataTests/SpaceTests/CardSpaceTests.cs
@@ -6,8 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using MonopolyKata.Board.Spaces;
+using MonopolyKata.Board;
 using MonopolyKata.Deck;
 using MonopolyKata.Cards;
+using MonopolyKata.Player;
 using Moq;
 
 namespace MonopolyKataTests.SpaceTests
@@ -16,11 +18,29 @@ namespace MonopolyKataTests.SpaceTests
     public class DeckSpaceTests
     {
         [Test]
-        public void ADeckSpaceShouldTakeADeck()
+        public void ACardSpaceShouldTakeADeck()
         {
             IDeck deck = new Deck(Moq.It.IsAny<Card>());
 
-            Assert.That(deck, Is.Not.Null);
+            IBoardSpace cardSpace = new CardSpace(Moq.It.IsAny<string>(), Moq.It.IsAny<MonopolyGameBoard>(), deck);
+
+            Assert.That(cardSpace, Is.Not.Null);
         }
+
+        [Test]
+        public void ACardSpaceShouldTakeDrawACardWhenLandedOn()
+        {
+            Mock<ICard> mockCard = new Mock<ICard>();
+
+            int count = 0;
+
+            mockCard.Setup(m => m.Draw(Moq.It.IsAny<MonopolyPlayer>())).Callback(() => { count++; });
+
+            IDeck deck = new Deck(mockCard.Object);
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+
     }
 }
